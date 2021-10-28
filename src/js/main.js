651b7f94f8cef1196272c46fefe486914a3364c2
timur <timurkakhkharov@gmail.com>
2021-10-29 03:33:10 +0500

    llo

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 
+    
+
+
 
 
 /******  ОТЗЫВЫ КЛИЕНТЫ   *****/
@@ -16,3 +19,32 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+
+
+
+
+// \
+
+const navLinks = document.querySelectorAll('.nav-link[data-goto]');
+
+if(navLinks.length > 0) {
+  navLinks.forEach(navLink => {
+    navLink.addEventListener("click", onNavLinkClick);
+  });
+
+
+  function onNavLinkClick(e) {
+    const navLink = e.target;
+    if(navLink.dataset.goto && document.querySelector(navLink.dataset.goto)){
+      const gotoBlock = document.querySelector(navLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('footer').offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+
+    }
+  }
+}
